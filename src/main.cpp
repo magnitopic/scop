4a34213d13f11745a39f377851c3b64126fea53e
magnitopic <magnitopic@gmail.com>
2026-07-03 12:46:54 +0200

    feat: compiling shaders

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f};
+
+const char *vertexShaderSource = "#version 330 core\n"
+								 "layout (location = 0) in vec3 aPos;\n"
+								 "void main()\n"
+								 "{\n"
+								 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+								 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+								   "out vec4 FragColor;\n"
+								   "void main()\n"
+								   "{\n"
+								   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+								   "}\0";
+
 int main()
 {
 	glfwInit();
@@ -49,6 +68,22 @@ int main()
 	{
 		glfwPollEvents();
 	}
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
